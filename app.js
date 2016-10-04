4cdd73d4979e8dfb11ad645340ea4190419e5268
Patrick Hernandez <r.patrick.hernandez@hotmail.com>
2016-10-03 23:12:29 -0400

    game logic added, style changes

--- app.js
+++ app.js
@@ -94,106 +94,100 @@ $('.box').on('click', function(){
 	if (board.allCols[col].s6 == false){
 
 		board.allCols[col].s6 = color
+		$('.c' + col + 's6').attr('data-color' , color);
 		$('.c' + col + 's6').css("background-color", color);
-		checkWin(col, 's6');
+		checkWin(col, '6', color);
 
 	} else if (board.allCols[col].s5 == false){
 
 		board.allCols[col].s5 = color
+		$('.c' + col + 's5').attr('data-color' , color);
 		$('.c' + col + 's5').css("background-color", color);
-		checkWin(col, 's5');
+		checkWin(col, '5', color);
 
 	} else if (board.allCols[col].s4 == false){
 
 		board.allCols[col].s4 = color
+		$('.c' + col + 's4').attr('data-color' , color);
 		$('.c' + col + 's4').css("background-color", color);
-		checkWin(col, 's4');
+		checkWin(col, '4', color);
 
 	} else if (board.allCols[col].s3 == false){
 
 		board.allCols[col].s3 = color
+		$('.c' + col + 's3').attr('data-color' , color);
 		$('.c' + col + 's3').css("background-color", color);
-		checkWin(col, 's3');
+		checkWin(col, '3', color);
 
 	} else if (board.allCols[col].s2 == false){
 
 		board.allCols[col].s2 = color
+		$('.c' + col + 's2').attr('data-color' , color);
 		$('.c' + col + 's2').css("background-color", color);
-		checkWin(col, 's2');
+		checkWin(col, '2', color);
 
 	} else if (board.allCols[col].s1 == false){
 
 		board.allCols[col].s1 = color
+		$('.c' + col + 's1').attr('data-color' , color);
 		$('.c' + col + 's1').css("background-color", color);
-		checkWin(col, 's1');
+		checkWin(col, '1', color);
 
 	} else if (board.allCols[col].s0 == false){
 
 		board.allCols[col].s0 = color
+		$('.c' + col + 's0').attr('data-color' , color);
 		$('.c' + col + 's0').css("background-color", color);
-		checkWin(col, 's0');
+		checkWin(col, '0', color);
 
 	} else {
 		alert('Row is full!');
 	};
 
-	checkWin();
-
 });
 
 
-function checkWin(col, spot){
-	var up1 = spot - 1;
-	var up2 = spot - 2;
-	var up3 = spot - 3;
+function checkWin(col, spot, color){
 
-	var left1 = col - 1
-	var left2 = col - 2;
-	var left3 = col - 3;
+	console.log(col, spot);
 
-	var right1 = col + 1;
-	var right2 = col + 2;
-	var right3 = col + 3;
+	if ($('.c' + col + 's' + spot).attr('data-color') == color && $('.c' + col + 's' + (parseInt(spot) - 1)).attr('data-color') == color && $('.c' + col + 's' + (parseInt(spot) - 2)).attr('data-color') == color && $('.c' + col + 's' + (parseInt(spot) - 3)).attr('data-color') == color){
+		win();
+	}
 
-	var lower1 = spot + 1;
-	var lower2 = spot + 2;
-	var lower3 = spot + 3;
+	if ($('.c' + col + 's' + spot).attr('data-color') == color && $('.c' + col + 's' + (parseInt(spot) + 1)).attr('data-color') == color && $('.c' + col + 's' + (parseInt(spot) + 2)).attr('data-color') == color && $('.c' + col + 's' + (parseInt(spot) + 3)).attr('data-color') == color){
+		win();
+	}
 
-	var diagUpperLeftCol1 = col - 1;
-	var diagUpperLeftCol2 = col - 2;
-	var diagUpperLeftCol3 = col - 3;
-	var diagUpperLeftSpot1 = spot - 1;
-	var diagUpperLeftSpot2 = spot - 2;
-	var diagUpperLeftSpot3 = spot - 3;
+	if ($('.c' + col + 's' + spot).attr('data-color') == color && $('.c' + (parseInt(col) + 1) + 's' + spot).attr('data-color') == color && $('.c' + (parseInt(col) + 2) + 's' + spot).attr('data-color') == color && $('.c' + (parseInt(col) + 3) + 's' + spot).attr('data-color') == color){
+		win();
+	}
 
-	var diagUpperRightCol1 = col + 1;
-	var diagUpperRightCol2 = col + 2;
-	var diagUpperRightCol3 = col + 3;
-	var diagUpperRightSpot1 = spot - 1;
-	var diagUpperRightSpot2 = spot - 2;
-	var diagUpperRightSpot3 = spot - 3;
+	if ($('.c' + col + 's' + spot).attr('data-color') == color && $('.c' + (parseInt(col) - 1) + 's' + spot).attr('data-color') == color && $('.c' + (parseInt(col) - 2) + 's' + spot).attr('data-color') == color && $('.c' + (parseInt(col) - 3) + 's' + spot).attr('data-color') == color){
+		win();
+	}
 
-	var diagLowerLeftCol1 = col - 1;
-	var diagLowerLeftCol2 = col - 2;
-	var diagLowerLeftCol3 = col - 3;
-	var diagLowerLeftSpot1 = spot + 1;
-	var diagLowerLeftSpot2 = spot + 2;
-	var diagLowerLeftSpot3 = spot + 3;
+	if ($('.c' + col + 's' + spot).attr('data-color') == color && $('.c' + (parseInt(col) + 1) + 's' + (parseInt(spot) + 1)).attr('data-color') == color && $('.c' + (parseInt(col) + 2) + 's' + (parseInt(spot) + 2)).attr('data-color') == color && $('.c' + (parseInt(col) + 3) + 's' + (parseInt(spot) + 3)).attr('data-color') == color){
+		win();
+	}
 
-	var diagLowerRightCol1 = col + 1;
-	var diagLowerRightCol2 = col + 2;
-	var diagLowerRightCol3 = col + 3;
-	var diagLowerRightSpot1 = spot + 1;
-	var diagLowerRightSpot2 = spot + 2;
-	var diagLowerRightSpot3 = spot + 3;
+	if ($('.c' + col + 's' + spot).attr('data-color') == color && $('.c' + (parseInt(col) - 1) + 's' + (parseInt(spot) - 1)).attr('data-color') == color && $('.c' + (parseInt(col) - 2) + 's' + (parseInt(spot) - 2)).attr('data-color') == color && $('.c' + (parseInt(col) - 3) + 's' + (parseInt(spot) - 3)).attr('data-color') == color){
+		win();
+	}
 
+	if ($('.c' + col + 's' + spot).attr('data-color') == color && $('.c' + (parseInt(col) + 1) + 's' + (parseInt(spot) - 1)).attr('data-color') == color && $('.c' + (parseInt(col) + 2) + 's' + (parseInt(spot) - 2)).attr('data-color') == color && $('.c' + (parseInt(col) + 3) + 's' + (parseInt(spot) - 3)).attr('data-color') == color){
+		win();
+	}
 
+	if ($('.c' + col + 's' + spot).attr('data-color') == color && $('.c' + (parseInt(col) - 1) + 's' + (parseInt(spot) + 1)).attr('data-color') == color && $('.c' + (parseInt(col) - 2) + 's' + (parseInt(spot) + 2)).attr('data-color') == color && $('.c' + (parseInt(col) - 3) + 's' + (parseInt(spot) + 3)).attr('data-color') == color){
+		win();
+	}
+
+}
+			
+function win(){
 	winDelay = setTimeout(function() {
-		if(
-		(board.allCols[0].s6 && board.allCols[0].s5 && board.allCols[0].s4 && board.allCols[0].s3) ||
-		(board.allCols[0].s5 && board.allCols[0].s4 && board.allCols[0].s3 && board.allCols[0].s2) ||
-		(board.allCols[0].s4 && board.allCols[0].s3 && board.allCols[0].s2 && board.allCols[0].s1) == ('red')){
-			alert(color + " wins!")
-		}
-	}, 100);
+		alert(color + ' wins!');
+		location.reload();
+	}, 100)
 }
